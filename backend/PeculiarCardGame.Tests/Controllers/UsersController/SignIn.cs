1d0f9aae173230b70d5b942ba6e40b7af4696d4a
Marcin Galiński <marcinggalinski@gmail.com>
2024-01-05 04:13:13 +0100

    add tests for SignIn endpoint

--- backend/PeculiarCardGame.Tests/Controllers/UsersController/SignIn.cs
+++ backend/PeculiarCardGame.Tests/Controllers/UsersController/SignIn.cs
@@ -4,21 +4,23 @@ using PeculiarCardGame.Data.Models;
 using PeculiarCardGame.Services.Authentication;
 using PeculiarCardGame.Services.DeckManagement;
 using PeculiarCardGame.Services.Users;
+using PeculiarCardGame.WebApi.Models.Responses;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text;
 
 namespace PeculiarCardGame.UnitTests.Controllers.UsersController
 {
-    public class DeleteUser
+    public class SignIn
     {
+        private const string BearerToken = "token";
+
         private readonly User _existingUser;
         private readonly User _notExistingUser;
 
-        private readonly IUsersService _usersService;
-
         private readonly HttpClient _client;
 
-        public DeleteUser()
+        public SignIn()
         {
             const int ExistingUserId = 1;
             const int NotExistingUserId = 2;
@@ -42,18 +44,15 @@ namespace PeculiarCardGame.UnitTests.Controllers.UsersController
                 PasswordHash = PasswordHash
             };
 
-            _usersService = Substitute.For<IUsersService>();
-            _usersService.GetUser(_existingUser.Id).Returns(_existingUser);
-            _usersService.GetUser(_notExistingUser.Id).Returns((User?)null);
-            _usersService.DeleteUser(_existingUser.Id).Returns(true);
-            _usersService.DeleteUser(_notExistingUser.Id).Returns(false);
-
             var authenticationService = Substitute.For<IAuthenticationService>();
-            authenticationService.Authenticate(Arg.Any<string>()).Returns(_existingUser);
+            authenticationService.Authenticate(_existingUser.Username, _existingUser.PasswordHash).Returns(_existingUser);
+            authenticationService.Authenticate(_notExistingUser.Username, _notExistingUser.PasswordHash).Returns((User?)null);
+            authenticationService.GenerateBearerToken().Returns(BearerToken);
 
+            var usersService = Substitute.For<IUsersService>();
             var deckManagementService = Substitute.For<IDeckManagementService>();
 
-            var clientFactory = new ApiTestsWebApplicationFactory<Program>(authenticationService, _usersService, deckManagementService);
+            var clientFactory = new ApiTestsWebApplicationFactory<Program>(authenticationService, usersService, deckManagementService);
             _client = clientFactory.CreateClient();
         }
 
@@ -62,59 +61,46 @@ namespace PeculiarCardGame.UnitTests.Controllers.UsersController
         {
             var message = await _client.SendAsync(new HttpRequestMessage
             {
-                Method = HttpMethod.Delete,
-                RequestUri = new Uri(_client.BaseAddress!, $"/api/users/{_existingUser.Id}")
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(_client.BaseAddress!, "api/users/signin")
             });
 
             message.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
 
         [Fact]
-        public async Task NotExistingUserId_ShouldReturnNotFound()
+        public async Task NotExistingUserCredentials_ShouldReturnUnauthorized()
         {
             var message = await _client.SendAsync(new HttpRequestMessage
             {
-                Method = HttpMethod.Delete,
-                RequestUri = new Uri(_client.BaseAddress!, $"/api/users/{_notExistingUser.Id}"),
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(_client.BaseAddress!, "api/users/signin"),
                 Headers =
                 {
-                    { HttpRequestHeader.Authorization.ToString(), "Bearer token" }
+                    { HttpRequestHeader.Authorization.ToString(), $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_notExistingUser.Username}:{_notExistingUser.PasswordHash}"))}" }
                 }
             });
 
-            message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            message.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
 
         [Fact]
-        public async Task ExistingUserId_ShouldCallDeleteUser()
-        {
-            await _client.SendAsync(new HttpRequestMessage
-            {
-                Method = HttpMethod.Delete,
-                RequestUri = new Uri(_client.BaseAddress!, $"/api/users/{_existingUser.Id}"),
-                Headers =
-                {
-                    { HttpRequestHeader.Authorization.ToString(), "Bearer token" }
-                }
-            });
-
-            _usersService.Received().DeleteUser(_existingUser.Id);
-        }
-
-        [Fact]
-        public async Task ExistingUserId_ShouldReturnOk()
+        public async Task ExistingUserId_ShouldReturnOkWithBearerToken()
         {
             var message = await _client.SendAsync(new HttpRequestMessage
             {
-                Method = HttpMethod.Delete,
-                RequestUri = new Uri(_client.BaseAddress!, $"/api/users/{_existingUser.Id}"),
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(_client.BaseAddress!, "api/users/signin"),
                 Headers =
                 {
-                    { HttpRequestHeader.Authorization.ToString(), "Bearer token" }
+                    { HttpRequestHeader.Authorization.ToString(), $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_existingUser.Username}:{_existingUser.PasswordHash}"))}" }
                 }
             });
+            var response = await message.Content.ReadFromJsonAsync<SignInResponse>();
 
             message.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Should().NotBeNull();
+            response!.Token.Should().Be(BearerToken);
         }
     }
 }
