f023ec21e8fb7e3e29261bc109d7cd2771be34fa
Marcin Galiński <marcing305@wp.pl>
2024-06-21 14:03:56 +0200

    use backend dev cert also for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,32 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import fs from "fs";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), mkcert()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    watch: {
-      usePolling: true,
+    server: {
+      port: 3000,
+      strictPort: true,
+      watch: {
+        usePolling: true,
+      },
+      hmr: {
+        host: "localhost",
+      },
+      https: {
+        pfx: fs.readFileSync(process.env.VITE_CERT_LOCATION!),
+        passphrase: process.env.VITE_CERT_PASSPHRASE,
+      },
     },
-    hmr: {
-      host: "localhost",
-    },
-  },
+  };
 });
