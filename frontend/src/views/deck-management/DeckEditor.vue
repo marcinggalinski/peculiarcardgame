85202fdd8e45ad559c23b4e4a50b9ef685b2bf4d
Marcin Galiński <marcing305@wp.pl>
2024-11-22 14:07:31 +0100

    style changes for edit buttons + fix discarding card text changes + improvements regarding updating

--- frontend/src/views/deck-management/DeckEditor.vue
+++ frontend/src/views/deck-management/DeckEditor.vue
@@ -1,85 +1,95 @@
 <template>
-  <h1 class="deck-name">
-    {{ deck.name }}
-    <Button
-      rounded
-      text
-      v-if="userStore.id == deck.authorId"
-      icon="pi pi-pencil"
-      class="edit-name-button"
-      @click="showEditNameDialog()"
+  <div id="deck-editor">
+    <h1 id="deck-name">
+      {{ deck.name }}
+      <Button
+        v-if="userStore.id == deck.authorId"
+        rounded
+        text
+        icon="pi pi-pencil"
+        id="edit-name-button"
+        @click="showEditNameDialog()"
+      />
+    </h1>
+    <small id="deck-author">
+      by {{ deck.author }}
+      <span v-if="userStore.id == deck.authorId">(you)</span>
+    </small>
+    <div id="deck-description">
+      {{ deck.description }}
+      <Button
+        v-if="userStore.id == deck.authorId"
+        rounded
+        text
+        icon="pi pi-pencil"
+        id="edit-description-button"
+        @click="showEditDescriptionDialog()"
+      />
+    </div>
+
+    <CardsList
+      :authorId="deck.authorId"
+      :type="CardType.Black"
+      :cards="blackCards"
+      :deleted-ids="updates.deletedCards"
+      @add="(text, type) => addCard(text, type)"
+      @update="(id, text) => updateCard(id, text)"
+      @delete="id => deleteCard(id)"
+      @restore="id => restoreCard(id)"
     />
-  </h1>
-  <small class="deck-author">
-    by {{ deck.author }}
-    <span v-if="userStore.id == deck.authorId">(you)</span>
-  </small>
-  <div class="deck-description">
-    {{ deck.description }}
-    <Button
-      rounded
-      text
-      v-if="userStore.id == deck.authorId"
-      icon="pi pi-pencil"
-      class="edit-description-button"
-      @click="showEditDescriptionDialog()"
+    <CardsList
+      :authorId="deck.authorId"
+      :type="CardType.White"
+      :cards="whiteCards"
+      :deleted-ids="updates.deletedCards"
+      @add="(text, type) => addCard(text, type)"
+      @update="(id, text) => updateCard(id, text)"
+      @delete="id => deleteCard(id)"
+      @restore="id => restoreCard(id)"
     />
+
+    <Dialog
+      modal
+      class="dialog"
+      v-model:visible="isEditNameDialogVisible"
+      header="Edit deck name"
+      :closable="false"
+      :draggable="false"
+    >
+      <InputText v-model="temp" class="edit-input" />
+      <template #footer>
+        <Button class="float-left" severity="secondary" label="Cancel" @click="hideEditNameDialog(false)" />
+        <Button class="float-right" label="Save" @click="hideEditNameDialog(true)" />
+      </template>
+    </Dialog>
+
+    <Dialog
+      modal
+      class="dialog"
+      v-model:visible="isEditDescriptionDialogVisible"
+      header="Edit deck description"
+      :closable="false"
+      :draggable="false"
+    >
+      <Textarea v-model="temp" class="edit-input" />
+      <template #footer>
+        <Button class="float-left" severity="secondary" label="Cancel" @click="hideEditDescriptionDialog(false)" />
+        <Button class="float-right" label="Save" @click="hideEditDescriptionDialog(true)" />
+      </template>
+    </Dialog>
+
+    <Toast id="pending-changes-toast" position="bottom-right" group="save-toast">
+      <template #message="slotProps">
+        <i class="pi pi-exclamation-triangle p-toast-message-icon" />
+        <div class="p-toast-message-text">
+          <span class="p-toast-summary">{{ slotProps.message.summary }}</span>
+          <div class="p-toast-detail">{{ slotProps.message.detail }}</div>
+        </div>
+        <Button label="Discard" severity="secondary" @click="discardChanges()" />
+        <Button label="Save" :loading="isUpdating" @click="updateDeck()" />
+      </template>
+    </Toast>
   </div>
-
-  <CardsList
-    :authorId="deck.authorId"
-    :type="CardType.Black"
-    :cards="blackCards"
-    :deleted-ids="updates.deletedCards"
-    @add="(text, type) => addCard(text, type)"
-    @update="(id, text) => updateCard(id, text)"
-    @delete="id => deleteCard(id)"
-    @restore="id => restoreCard(id)"
-  />
-  <CardsList
-    :authorId="deck.authorId"
-    :type="CardType.White"
-    :cards="whiteCards"
-    :deleted-ids="updates.deletedCards"
-    @add="(text, type) => addCard(text, type)"
-    @update="(id, text) => updateCard(id, text)"
-    @delete="id => deleteCard(id)"
-    @restore="id => restoreCard(id)"
-  />
-
-  <Dialog modal class="dialog" v-model:visible="isEditNameDialogVisible" header="Edit deck name" :closable="false">
-    <InputText v-model="temp" class="edit-input" />
-    <template #footer>
-      <Button class="float-left" severity="secondary" label="Cancel" @click="hideEditNameDialog(false)" />
-      <Button class="float-right" label="Save" @click="hideEditNameDialog(true)" />
-    </template>
-  </Dialog>
-
-  <Dialog
-    modal
-    class="dialog"
-    v-model:visible="isEditDescriptionDialogVisible"
-    header="Edit deck description"
-    :closable="false"
-  >
-    <Textarea v-model="temp" class="edit-input" />
-    <template #footer>
-      <Button class="float-left" severity="secondary" label="Cancel" @click="hideEditDescriptionDialog(false)" />
-      <Button class="float-right" label="Save" @click="hideEditDescriptionDialog(true)" />
-    </template>
-  </Dialog>
-
-  <Toast id="pending-changes-toast" position="bottom-right" group="save-toast">
-    <template #message="slotProps">
-      <i class="pi pi-exclamation-triangle p-toast-message-icon" />
-      <div class="p-toast-message-text">
-        <span class="p-toast-summary">{{ slotProps.message.summary }}</span>
-        <div class="p-toast-detail">{{ slotProps.message.detail }}</div>
-      </div>
-      <Button label="Discard" severity="secondary" @click="discardChanges()" />
-      <Button label="Save" :loading="isUpdating" @click="updateDeck()" />
-    </template>
-  </Toast>
 </template>
 
 <script setup lang="ts">
@@ -90,6 +100,7 @@ import Dialog from "primevue/dialog";
 import InputText from "primevue/inputtext";
 import Textarea from "primevue/textarea";
 import Toast from "primevue/toast";
+import { useToast } from "primevue/usetoast";
 
 import CardsList from "@/components/deck-management/CardsList.vue";
 import { DeckManagementApiServiceKey } from "@/keys";
@@ -97,7 +108,6 @@ import type { GetCardResponse, GetDeckResponse } from "@/models/deck-management/
 import { CardType } from "@/models/deck-management/api";
 import DeckManagementApiService from "@/services/deck-management/apiService";
 import { useUserStore } from "@/stores/user";
-import { useToast } from "primevue/usetoast";
 
 const { id } = defineProps<{
   id: number;
@@ -146,9 +156,10 @@ const deck = computed<GetDeckResponse>(() => {
   };
 });
 const cards = computed<GetCardResponse[]>(() => {
-  const cards = data.cards.concat(updates.addedCards);
+  const cardsCopy = JSON.parse(JSON.stringify(data.cards)) as GetCardResponse[];
+  const cards = cardsCopy.concat(updates.addedCards);
   for (const update of updates.cards) {
-    cards.find(x => x.id == update.id)!.text = update.text;
+    cards.find(x => x.id === update.id)!.text = update.text;
   }
   return cards;
 });
@@ -178,7 +189,9 @@ const showEditNameDialog = () => {
 
 const hideEditNameDialog = (save: boolean) => {
   if (save) {
-    updates.name = temp.value.trim();
+    const trimmed = temp.value.trim();
+    if (trimmed === data.deck.name) delete updates.name;
+    else if (trimmed !== updates.name) updates.name = trimmed;
   }
   isEditNameDialogVisible.value = false;
 };
@@ -190,7 +203,9 @@ const showEditDescriptionDialog = () => {
 
 const hideEditDescriptionDialog = (save: boolean) => {
   if (save) {
-    updates.description = temp.value.trim();
+    const trimmed = temp.value.trim();
+    if (trimmed === data.deck.description) delete updates.description;
+    else if (trimmed !== updates.description) updates.description = trimmed;
   }
   isEditDescriptionDialogVisible.value = false;
 };
@@ -200,23 +215,36 @@ const addCard = (text: string, cardType: CardType) => {
 };
 
 const updateCard = (id: number, text: string) => {
-  let card = updates.cards.find(x => x.id == id);
-  if (card) {
-    card.text = text;
+  const trimmed = text.trim();
+
+  const addedCard = updates.addedCards.find(x => x.id === id);
+  if (addedCard && trimmed !== addedCard.text) {
+    addedCard.text = trimmed;
     return;
   }
 
-  card = updates.addedCards.find(x => x.id == id);
-  if (card) {
-    card.text = text;
+  const originalCard = data.cards.find(x => x.id === id)!;
+  const updatedCard = updates.cards.find(x => x.id === id);
+  if (updatedCard) {
+    if (trimmed === originalCard.text) {
+      const index = updates.cards.indexOf(updatedCard);
+      updates.cards.splice(index, 1);
+      return;
+    }
+
+    if (trimmed === updatedCard.text) return;
+
+    updatedCard.text = trimmed;
     return;
   }
 
-  updates.cards.push({ id, text });
+  if (trimmed === originalCard.text) return;
+
+  updates.cards.push({ id, text: trimmed });
 };
 
 const deleteCard = (id: number) => {
-  const index = updates.addedCards.findIndex(x => x.id == id);
+  const index = updates.addedCards.findIndex(x => x.id === id);
   if (index > -1) {
     updates.addedCards.splice(index, 1);
     return;
@@ -292,34 +320,70 @@ await getData();
 </script>
 
 <style lang="stylus">
-.deck-name
-  margin-bottom 0
+#deck-editor
+  padding 10px
 
-.deck-description
-  white-space pre
+  #deck-name
+    position relative
+    margin-bottom 0
+    padding-bottom 0
+    width fit-content
 
-.deck-editor-edit-title
-  cursor pointer
-  border 1px solid black
-  border-radius 50%
-  padding 5px
+    #edit-name-button
+      position absolute
+      top 0
+      right 0
 
-.deck-editor-edit-description
-  cursor pointer
+      background none
+      transition 0.3s ease
+      opacity 0
 
-.edit-name-button
-  height 39px
-  width 39px
+      height 39px
+      width 39px
 
-.edit-description-button
-  height 39px
-  width 39px
+      transform translate(100%, 0)
+
+  #deck-name:hover
+    #edit-name-button
+      transition 0.3s ease
+      opacity 1
+
+  #deck-author
+    padding-left 25px
+
+  #deck-description
+    position relative
+    margin-top 15px
+    padding 5px
+    white-space pre-wrap
+    border 1px solid black
+    border-radius 10px
+
+    #edit-description-button
+      position absolute
+      top 0
+      right 0
+
+      background none
+      transition 0.3s ease
+      opacity 0
+
+      height 39px
+      width 39px
+
+  #deck-description:hover
+    #edit-description-button
+      transition 0.3s ease
+      opacity 1
 
 .dialog
   width 50%
 
   .edit-input
     width 100%
+
+  .p-inputtextarea.edit-input
+    min-height 250px
 
 #pending-changes-toast
   width fit-content
